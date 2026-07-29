fe73d7ee4b60b7a7fc31ff25e55a328197589d3d
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-29 01:06:55 +0100

    commit practice

---
+++ Practise/console.js
@@ -0,0 +1,14 @@
+console.log(0.3 + 0.3 === 0.6);
+const obj = {
+  a: 1,
+  b: 2,
+};
+
+console.log(obj.toString());
+
+const str = JSON.stringify(obj);
+console.log(str);
+
+let arr = [1,2,3,4];
+
+const result = arr.map
